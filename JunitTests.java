6e104cfd9d090fd2a4e3bdd3431d69e2ef5d0102
HarryBurdick <harry.burdick@campus.mccd.edu>
2024-11-09 07:46:21 +0000

    Fixed formatting

--- JunitTests.java
+++ JunitTests.java
@@ -1,118 +1,144 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-
-public class JunitTests{
+public class JunitTests {
     @Test
-    public void testCount1(){
+    public void testCount1() {
         assertEquals(1, RecursionProblems.Recursion1(8));
     }
+
     @Test
-    public void testCount2(){
+    public void testCount2() {
         assertEquals(2, RecursionProblems.Recursion1(818));
     }
+
     @Test
-    public void testCount3(){
+    public void testCount3() {
         assertEquals(4, RecursionProblems.Recursion1(8818));
     }
+
     @Test
-    public void testCount4(){
+    public void testCount4() {
         assertEquals(6, RecursionProblems.Recursion1(88188));
     }
+
     @Test
-    public void testCount5(){
+    public void testCount5() {
         assertEquals(9, RecursionProblems.Recursion1(88888));
     }
+
     @Test
-    public void testCount6(){
+    public void testCount6() {
         assertEquals(1, RecursionProblems.Recursion1(12345678));
     }
+
     @Test
-    public void testCount7(){
+    public void testCount7() {
         assertEquals(0, RecursionProblems.Recursion1(123456790));
     }
+
     @Test
-    public void CountHi1(){
+    public void CountHi1() {
         assertEquals(1, RecursionProblems.Recursion2("xxhixx"));
     }
+
     @Test
-    public void CountHi2(){
+    public void CountHi2() {
         assertEquals(1, RecursionProblems.Recursion2("xhixhix"));
     }
+
     @Test
-    public void CountHi3(){
+    public void CountHi3() {
         assertEquals(1, RecursionProblems.Recursion2("hi"));
     }
+
     @Test
-    public void CountHi4(){
+    public void CountHi4() {
         assertEquals(0, RecursionProblems.Recursion2("xxxx"));
     }
+
     @Test
-    public void CountHi5(){
+    public void CountHi5() {
         assertEquals(3, RecursionProblems.Recursion2("hihihi"));
     }
+
     @Test
-    public void CountxHi1(){
+    public void CountxHi1() {
         assertEquals(1, RecursionProblems.Recursion3("ahixhi"));
     }
+
     @Test
-    public void CountxHi2(){
+    public void CountxHi2() {
         assertEquals(2, RecursionProblems.Recursion3("ahibhi"));
     }
+
     @Test
-    public void CountxHi3(){
+    public void CountxHi3() {
         assertEquals(0, RecursionProblems.Recursion3("xhixhi"));
     }
+
     @Test
-    public void CountxHi4(){
+    public void CountxHi4() {
         assertEquals(0, RecursionProblems.Recursion3("xhixxxxhi"));
     }
+
     @Test
-    public void CountxHi5(){
+    public void CountxHi5() {
         assertEquals(2, RecursionProblems.Recursion3("hihixxhi"));
     }
+
     @Test
-    public void CountSub1(){
+    public void CountSub1() {
         assertEquals(2, RecursionProblems.Recursion4("catcowcat", "cat"));
     }
+
     @Test
-    public void CountSub2(){
+    public void CountSub2() {
         assertEquals(1, RecursionProblems.Recursion4("catcowcat", "cow"));
     }
+
     @Test
-    public void CountSub3(){
+    public void CountSub3() {
         assertEquals(0, RecursionProblems.Recursion4("catcowcat", "dog"));
     }
+
     @Test
-    public void CountSub4(){
+    public void CountSub4() {
         assertEquals(2, RecursionProblems.Recursion4("appaaaapp", "app"));
     }
+
     @Test
-    public void CountSub5(){
+    public void CountSub5() {
         assertEquals(2, RecursionProblems.Recursion4("dogbowdogdog", "dog"));
     }
+
     @Test
-    public void CountChar1(){
+    public void CountChar1() {
         assertEquals("yza", RecursionProblems.Recursion5("yyzzza"));
     }
+
     @Test
-    public void CountChar2(){
+    public void CountChar2() {
         assertEquals("abcd", RecursionProblems.Recursion5("abbbcdd"));
     }
+
     @Test
-    public void CountChar3(){
+    public void CountChar3() {
         assertEquals("Helo", RecursionProblems.Recursion5("Hello"));
     }
+
     @Test
-    public void CountChar4(){
+    public void CountChar4() {
         assertEquals("abcd", RecursionProblems.Recursion5("abcd"));
     }
+
     @Test
-    public void CountChar5(){
+    public void CountChar5() {
         assertEquals("uibcd", RecursionProblems.Recursion5("uuiiibcdd"));
     }
+
     @Test
-    public void CountChar6(){
+    public void CountChar6() {
         assertEquals("ababababababa", RecursionProblems.Recursion5("abbbababaabbbbbababaaa"));
     }
 }
--- RecursionProblems.java
+++ RecursionProblems.java
@@ -1,24 +1,25 @@
 public class RecursionProblems {
-    public static int Recursion1(int n){
-        //base case if n == 0, no 8s are currently present
-        if(n == 0) return 0;
+    public static int Recursion1(int n) {
+        // base case if n == 0, no 8s are currently present
+        if (n == 0)
+            return 0;
 
         // if the last digit has an 8
-        if (n % 10 == 8){
+        if (n % 10 == 8) {
             // checks if the second to last digit present is also an 8
-            if ((n / 10) % 10 == 8){
+            if ((n / 10) % 10 == 8) {
                 // if it is then count it double and continue recursively with the rest
                 return 2 + Recursion1(n / 10);
-            }else{
-                //otherwise count it as a 1 and continue recursively with the rest
+            } else {
+                // otherwise count it as a 1 and continue recursively with the rest
                 return 1 + Recursion1(n / 10);
             }
-        }else{
+        } else {
             // if the final digit is not an 8, continue with the rest of the number given
             return Recursion1(n / 10);
         }
     }
-    
+
     public static int Recursion2(String str) {
         if (str.length() < 2)
             return 0;
