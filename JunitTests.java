b71ce7ecdaabef90d11ecfda4a7c4e6c6c1726e1
HarryBurdick <harry.burdick@campus.mccd.edu>
2024-11-09 07:45:22 +0000

    Reformatted to include all recursion problems into one file & refactored all Junit Tests

--- JunitTests.java
+++ JunitTests.java
@@ -5,114 +5,114 @@ import org.junit.jupiter.api.Test;
 public class JunitTests{
     @Test
     public void testCount1(){
-        assertEquals(1, RecursionProblem1.Recursion1(8), "Recursion1(8) should return 1");
+        assertEquals(1, RecursionProblems.Recursion1(8));
     }
     @Test
     public void testCount2(){
-        assertEquals(2, RecursionProblem1.Recursion1(818), "Recursion1(818) should return 2");
+        assertEquals(2, RecursionProblems.Recursion1(818));
     }
     @Test
     public void testCount3(){
-        assertEquals(4, RecursionProblem1.Recursion1(8818), "Recursion1(8818) should return 4");
+        assertEquals(4, RecursionProblems.Recursion1(8818));
     }
     @Test
     public void testCount4(){
-        assertEquals(6, RecursionProblem1.Recursion1(88188), "Recursion1(8818) should return 4");
+        assertEquals(6, RecursionProblems.Recursion1(88188));
     }
     @Test
     public void testCount5(){
-        assertEquals(9, RecursionProblem1.Recursion1(88888), "Recursion1(8818) should return 4");
+        assertEquals(9, RecursionProblems.Recursion1(88888));
     }
     @Test
     public void testCount6(){
-        assertEquals(1, RecursionProblem1.Recursion1(12345678), "Recursion1(8818) should return 4");
+        assertEquals(1, RecursionProblems.Recursion1(12345678));
     }
     @Test
     public void testCount7(){
-        assertEquals(0, RecursionProblem1.Recursion1(123456790), "Recursion1(8818) should return 4");
+        assertEquals(0, RecursionProblems.Recursion1(123456790));
     }
     @Test
     public void CountHi1(){
-        assertEquals(1, RecursionProblem2.Recursion2("xxhixx"));
+        assertEquals(1, RecursionProblems.Recursion2("xxhixx"));
     }
     @Test
     public void CountHi2(){
-        assertEquals(1, RecursionProblem2.Recursion2("xhixhix"));
+        assertEquals(1, RecursionProblems.Recursion2("xhixhix"));
     }
     @Test
     public void CountHi3(){
-        assertEquals(1, RecursionProblem2.Recursion2("hi"));
+        assertEquals(1, RecursionProblems.Recursion2("hi"));
     }
     @Test
     public void CountHi4(){
-        assertEquals(0, RecursionProblem2.Recursion2("xxxx"));
+        assertEquals(0, RecursionProblems.Recursion2("xxxx"));
     }
     @Test
     public void CountHi5(){
-        assertEquals(3, RecursionProblem2.Recursion2("hihihi"));
+        assertEquals(3, RecursionProblems.Recursion2("hihihi"));
     }
     @Test
     public void CountxHi1(){
-        assertEquals(1, RecursionProblem2.Recursion3("ahixhi"));
+        assertEquals(1, RecursionProblems.Recursion3("ahixhi"));
     }
     @Test
     public void CountxHi2(){
-        assertEquals(2, RecursionProblem2.Recursion3("ahibhi"));
+        assertEquals(2, RecursionProblems.Recursion3("ahibhi"));
     }
     @Test
     public void CountxHi3(){
-        assertEquals(0, RecursionProblem2.Recursion3("xhixhi"));
+        assertEquals(0, RecursionProblems.Recursion3("xhixhi"));
     }
     @Test
     public void CountxHi4(){
-        assertEquals(0, RecursionProblem2.Recursion3("xhixxxxhi"));
+        assertEquals(0, RecursionProblems.Recursion3("xhixxxxhi"));
     }
     @Test
     public void CountxHi5(){
-        assertEquals(2, RecursionProblem2.Recursion3("hihixxhi"));
+        assertEquals(2, RecursionProblems.Recursion3("hihixxhi"));
     }
     @Test
     public void CountSub1(){
-        assertEquals(2, RecursionProblem2.Recursion4("catcowcat", "cat"));
+        assertEquals(2, RecursionProblems.Recursion4("catcowcat", "cat"));
     }
     @Test
     public void CountSub2(){
-        assertEquals(1, RecursionProblem2.Recursion4("catcowcat", "cow"));
+        assertEquals(1, RecursionProblems.Recursion4("catcowcat", "cow"));
     }
     @Test
     public void CountSub3(){
-        assertEquals(0, RecursionProblem2.Recursion4("catcowcat", "dog"));
+        assertEquals(0, RecursionProblems.Recursion4("catcowcat", "dog"));
     }
     @Test
     public void CountSub4(){
-        assertEquals(2, RecursionProblem2.Recursion4("appaaaapp", "app"));
+        assertEquals(2, RecursionProblems.Recursion4("appaaaapp", "app"));
     }
     @Test
     public void CountSub5(){
-        assertEquals(2, RecursionProblem2.Recursion4("dogbowdogdog", "dog"));
+        assertEquals(2, RecursionProblems.Recursion4("dogbowdogdog", "dog"));
     }
     @Test
     public void CountChar1(){
-        assertEquals("yza", RecursionProblem2.Recursion5("yyzzza"));
+        assertEquals("yza", RecursionProblems.Recursion5("yyzzza"));
     }
     @Test
     public void CountChar2(){
-        assertEquals("abcd", RecursionProblem2.Recursion5("abbbcdd"));
+        assertEquals("abcd", RecursionProblems.Recursion5("abbbcdd"));
     }
     @Test
     public void CountChar3(){
-        assertEquals("Helo", RecursionProblem2.Recursion5("Hello"));
+        assertEquals("Helo", RecursionProblems.Recursion5("Hello"));
     }
     @Test
     public void CountChar4(){
-        assertEquals("abcd", RecursionProblem2.Recursion5("abcd"));
+        assertEquals("abcd", RecursionProblems.Recursion5("abcd"));
     }
     @Test
     public void CountChar5(){
-        assertEquals("uibcd", RecursionProblem2.Recursion5("uuiiibcdd"));
+        assertEquals("uibcd", RecursionProblems.Recursion5("uuiiibcdd"));
     }
     @Test
     public void CountChar6(){
-        assertEquals("ababababababa", RecursionProblem2.Recursion5("abbbababaabbbbbababaaa"));
+        assertEquals("ababababababa", RecursionProblems.Recursion5("abbbababaabbbbbababaaa"));
     }
 }
