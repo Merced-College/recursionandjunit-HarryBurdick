aaf6302fbcf0363edc65433533c5efaf8fffd87f
HarryBurdick <harry.burdick@campus.mccd.edu>
2024-11-09 07:25:22 +0000

    Finished remaining recursion problems and included their Junit Tests

--- JunitTests.java
+++ JunitTests.java
@@ -15,4 +15,52 @@ public class JunitTests{
     public void testCount3(){
         assertEquals(4, RecursionProblem1.Recursion1(8818), "Recursion1(8818) should return 4");
     }
+    @Test
+    public void CountHi1(){
+        assertEquals(1, RecursionProblem2.Recursion2("xxhixx"));
+    }
+    @Test
+    public void CountHi2(){
+        assertEquals(1, RecursionProblem2.Recursion2("xhixhix"));
+    }
+    @Test
+    public void CountHi3(){
+        assertEquals(1, RecursionProblem2.Recursion2("hi"));
+    }
+    @Test
+    public void CountxHi1(){
+        assertEquals(1, RecursionProblem2.Recursion3("ahixhi"));
+    }
+    @Test
+    public void CountxHi2(){
+        assertEquals(2, RecursionProblem2.Recursion3("ahibhi"));
+    }
+    @Test
+    public void CountxHi3(){
+        assertEquals(0, RecursionProblem2.Recursion3("xhixhi"));
+    }
+    @Test
+    public void CountSub1(){
+        assertEquals(2, RecursionProblem2.Recursion4("catcowcat", "cat"));
+    }
+    @Test
+    public void CountSub2(){
+        assertEquals(1, RecursionProblem2.Recursion4("catcowcat", "cow"));
+    }
+    @Test
+    public void CountSub3(){
+        assertEquals(0, RecursionProblem2.Recursion4("catcowcat", "dog"));
+    }
+    @Test
+    public void CountChar1(){
+        assertEquals("yza", RecursionProblem2.Recursion5("yyzzza"));
+    }
+    @Test
+    public void CountChar2(){
+        assertEquals("abcd", RecursionProblem2.Recursion5("abbbcdd"));
+    }
+    @Test
+    public void CountChar3(){
+        assertEquals("Helo", RecursionProblem2.Recursion5("Hello"));
+    }
 }
--- RecursionProblem2.java
+++ RecursionProblem2.java
@@ -1,4 +1,55 @@
-public class RecursionProblem2{
-    
+public class RecursionProblem2 {
+    public static int Recursion2(String str) {
+        if (str.length() < 2)
+            return 0;
 
+        if (str.substring(0, 2).equals("hi")) {
+            return Recursion2(str.substring(2)) + 1;
+        }
+        return Recursion2(str.substring(2));
+
+    }
+
+    public static int Recursion3(String str) {
+        if (str.length() < 3)
+            return 0;
+
+        if (!str.startsWith("x") && str.contains("hi")) {
+            return Recursion3(str.substring(3)) + 1;
+        }
+        return Recursion3(str.substring(3));
+
+    }
+
+    public static int Recursion4(String str, String sub) {
+        if (str.length() < 3)
+            return 0;
+
+        if (str.substring(0, 3).contains(sub)) {
+            if (str.length() >= 6) {
+                String nextSub = str.substring(3);
+                if (nextSub.contains(sub)) {
+                    return Recursion4(nextSub.substring(3), sub) + 1;
+                } else {
+                    return Recursion4(str.substring(3), sub) + 1;
+                }
+            } else {
+                return Recursion4(str.substring(3), sub) + 1;
+            }
+        }
+        return Recursion4(str.substring(3), sub);
+
+    }
+
+    public static String Recursion5(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+        if (str.charAt(0) == str.charAt(1)) {
+
+            return Recursion5(str.substring(1));
+        } else {
+            return str.charAt(0) + Recursion5(str.substring(1));
+        }
+    }
 }
